0710f0b5dacda56b258489ec0dfc3140d04f5351
Juan Ruiz <jaruizdavalos@gmail.com>
2024-06-14 07:36:52 -0300

    first part, read json file

---
+++ Cap-1/fileJson.py
@@ -0,0 +1,7 @@
+import json
+
+route1="../../Big Data with Python/BigDataPython-master/data/Cap1/subvenciones.json"
+route2="../../Big Data with Python/BigDataPython-master/data/Cap1/subvenciones_agrupadas.json"
+
+
+with open(route1, encoding='uft-8') as fich_read, 
